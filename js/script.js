cc41295d50d7d5f547a77092382c6a09319ebf77
anticni <n.antic91@gmail.com>
2017-12-19 21:18:43 +0100

    switched style to buttons, added different check function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var cells = ["a1","a2","a3","b1","b2","b3","c1","c2","c3"];
-var playerone = ["11","11","11","b1","b2","b3","c1","c2","c3"];
-var player = 1
-var count = 1
+var playerone = [];
+var playertwo = [];
+var player = 1;
+var count = 1;
 
 $('#startbutton').click(function() {
     location.reload();
@@ -17,60 +18,64 @@ function play(el){
 switch(player){
 	case 1:
 	$(el).html("X");
+
 	$.each(cells, function(i){
 	if (cells[i] == el.id){
-		cells[i] = "11"
+		cells[i] = "x";
+		playerone.push(el.id);
 	}
 });
 	player = 2;
 	count = count+1;
-	$(el).attr('onclick','');
+	$(el).addClass('disabled');
 	break;
 
 	case 2:
 	$(el).html("O");
 	$.each(cells, function(i){
 	if (cells[i] == el.id){
-		cells[i] = "22"
+		cells[i] = "o"
+		playertwo.push(el.id);
 	}
 });
 	player = 1;
 	count=count+1;
-	$(el).attr('onclick','');
+	$(el).addClass('disabled');
 	break;
 }
+
 check();
 }
 
-function check(){
-	if(cells[0] == cells[1] && cells[1] == cells[2]){
-		alert("win, 012")
-	}
-	else if(cells[3] == cells[4] && cells[4] == cells[5]){
-		alert("win, 345")
-	}
-	else if(cells[6] == cells[7] && cells[7] == cells[8]){
-		alert("win, 678")
-	}
-	else if(cells[6] == cells[4] && cells[4] == cells[2]){
-		alert("win, 642")
-	}
-	else if(cells[0] == cells[3] && cells[3] == cells[6]){
-		alert("win, ")
-	}
-	else if(cells[1] == cells[4] && cells[4] == cells[7]){
-		alert("win, levo")
-	}
-	else if(cells[2] == cells[5] && cells[5] == cells[8]){
-		alert("win, levo")
-	}
-	else if(cells[0] == cells[4] && cells[4] == cells[8]){
-		alert("win, levo")
-	}
-	else if(count>9){
-		alert("draw");
-	}
-	}
+// function check(){
+// 	if(cells[0] == cells[1] && cells[1] == cells[2]){
+// 		alert("win, 012" + cells[0])
+// 	}
+// 	else if(cells[3] == cells[4] && cells[4] == cells[5]){
+// 		alert("win, 345")
+// 	}
+// 	else if(cells[6] == cells[7] && cells[7] == cells[8]){
+// 		alert("win, 678")
+// 	}
+// 	else if(cells[6] == cells[4] && cells[4] == cells[2]){
+// 		alert("win, 642")
+// 	}
+// 	else if(cells[0] == cells[3] && cells[3] == cells[6]){
+// 		alert("win, ")
+// 	}
+// 	else if(cells[1] == cells[4] && cells[4] == cells[7]){
+// 		alert("win, levo")
+// 	}
+// 	else if(cells[2] == cells[5] && cells[5] == cells[8]){
+// 		alert("win, levo")
+// 	}
+// 	else if(cells[0] == cells[4] && cells[4] == cells[8]){
+// 		alert("win, levo")
+// 	}
+// 	else if(count>9){
+// 		alert("draw");
+// 	}
+// 	}
 // var comb = [
 //   [0,1,2],
 //   [3,4,5],
@@ -82,3 +87,37 @@ function check(){
 //   [0,4,8]
 //   ];
 
+
+var comb = [
+	["a1","a2","a3"],
+	["b1","b2","b3"],
+	["c1","c2","c3"],
+	["a1","b2","c3"],
+	["a3","b2","c1"],
+	["a1","b1","c1"],
+	["a2","b2","c2"],
+	["a3","b3","c3"],
+	];
+
+function check(){
+	$.each(comb, function(j){
+		if (containsAll(comb[j], playerone) == true){
+			alert("win player X");
+			console.log("winning combo is" + comb[j])
+		}
+		else if(containsAll(comb[j], playertwo) == true){
+			alert("win player O")
+		}
+		else{console.log("no")}
+
+
+});
+}
+
+
+function containsAll(win, p){ 
+  for(var i = 0 , len = win.length; i < len; i++){
+     if($.inArray(win[i], p) == -1) return false;
+  }
+  return true;
+}
